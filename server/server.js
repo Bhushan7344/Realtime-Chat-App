569ce002a2856d0d3ab528f0e17bec6326104d60
D Bhushan <gbhushand@gmail.com>
2023-10-10 22:13:39 +0530

    fixed deployment errors

--- server/server.js
+++ server/server.js
@@ -11,8 +11,19 @@ const path = require("path");
 
 dbConnect();
 const app = express();
+const server = require("http").Server(app); // Use http.Server to create an HTTP server instance
+const io = require("socket.io")(server, {
+  pingTimeout: 60000,
+  cors: {
+    origin: "https://realtime-chat-app-kappa-ten.vercel.app",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Origin", "X-Requested-With", "Content-Type", "Accept"],
+  },
+});
+
 app.use(express.json());
 
+// CORS middleware to set headers
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'https://realtime-chat-app-kappa-ten.vercel.app');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
@@ -26,57 +37,53 @@ app.use("/api/chats", chatRoutes);
 app.use("/api/message", messageRoutes);
 app.use("/api/notification", notificationRoutes);
 
-
-  app.get("/", (req, res) => {
-    res.status(200).json({
-      message: "Hello World",
-    });
+app.get("/", (req, res) => {
+  res.status(200).json({
+    message: "Hello World",
   });
-
-
+});
 
 app.use(routeNotFound);
 app.use(errorHandler);
 
-const server = app.listen(process.env.PORT || 5000, () => {
-  console.log(`Server running on port: ${process.env.PORT}`);
-});
-
-const io = require("socket.io")(server, {
-  pingTimeout: 60000,
-  cors: {
-    origin: "https://realtime-chat-app-kappa-ten.vercel.app",
-  },
+server.listen(process.env.PORT || 5000, () => {
+  console.log(`Server running on port: ${process.env.PORT || 5000}`);
 });
 
 io.on("connection", (socket) => {
   console.log("Sockets are in action");
   socket.on("setup", (userData) => {
     socket.join(userData._id);
-    console.log(userData.name, "connected");
+    console.log(`${userData.name} connected`);
     socket.emit("connected");
   });
+
   socket.on("join chat", (room) => {
     socket.join(room);
     console.log("User joined room: " + room);
   });
+
   socket.on("new message", (newMessage) => {
-    var chat = newMessage.chatId;
+    const chat = newMessage.chatId;
     if (!chat.users) return console.log("chat.users not defined");
 
     chat.users.forEach((user) => {
       if (user._id === newMessage.sender._id) return;
       socket.in(user._id).emit("message received", newMessage);
     });
-    socket.on("typing", (room) => {
-      socket.in(room).emit("typing");
-    });
-    socket.on("stop typing", (room) => {
-      socket.in(room).emit("stop typing");
-    });
   });
-  socket.off("setup", () => {
-    console.log("USER DISCONNECTED");
-    socket.leave(userData._id);
+
+  socket.on("typing", (room) => {
+    socket.in(room).emit("typing");
+  });
+
+  socket.on("stop typing", (room) => {
+    socket.in(room).emit("stop typing");
+  });
+
+  // Handle disconnection
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
+    // You may want to add code to handle user disconnection, such as leaving rooms, cleaning up, etc.
   });
 });
